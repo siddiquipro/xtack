54a8851683745abb26e551525015bc8762253d3e
MadhavGutte <guttemadhav@gmail.com>
2025-06-11 23:06:23 +0530

    fixed an error of is not a string, by adding external depencies to rollup externals

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,9 @@
 import * as utils from "@poppinss/utils";
+import * as assertModule from "@poppinss/utils/assert";
 import string from "./string.js";
 
 export { Encryption } from "@adonisjs/encryption";
 
-type Helpers = typeof utils & { string: typeof string };
+type Helpers = typeof utils & { string: typeof string, assert: typeof  assertModule.assert };
 
-export const helpers = { ...utils, string } as Helpers;
+export const helpers = { ...utils, assert: assertModule.assert, string } as Helpers;
